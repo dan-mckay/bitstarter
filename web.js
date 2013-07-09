093a1d12d77183e6dea5d3589f509ce1247a45ff
Daniel McKay <mckayefc@gmail.com>
2013-07-09 22:23:21 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexBuf = fs.readFileSync('./index.html','utf8');
+  response.send(indexBuf);
 });
 
 var port = process.env.PORT || 5000;
